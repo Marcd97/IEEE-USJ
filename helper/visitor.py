ad538706dda9f521df049ba7e0b1b4ced5d8ee70
Marc Dib <marc.dib15@gmail.com>
2018-11-25 23:55:06 +0200

    Major Visitor Updates with Base Model
    Updated the Write and Read visitor to reflect more the Visitor Pattern, it will simply call "accept_read/write_visitor" on each instance.
    Base Model now has "accept_read_visitor" and "accept_write_visitor" methods that can optionally be overwritten in some subclasses to adapt (example: Members will overwrite it to implement the societies mapping)

--- helper/visitor.py
+++ helper/visitor.py
@@ -1,21 +1,10 @@
-from configuration.exceptions import ModelFieldError
-
-
 class ModelWriteVisitor:
     @staticmethod
-    def visit_model(instance, body):
-        for name in body:
-            try:
-                getattr(instance, name)
-            except AttributeError:
-                raise ModelFieldError("Field {} is not listed for this model".format(name))
-            else:
-                setattr(instance, name, body[name])
-        return instance
+    def visit(instance, body):
+        return instance.accept_write_visitor(body)
 
 
 class ModelReadVisitor:
     @staticmethod
     def visit_model(instance):
-        instance_dict = instance.as_dict(instance.crud_metadata)
-        return instance_dict
+        return instance.accept_read_visitor(instance.crud_metadata)
--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@ __all__ = (
 )
 
 from app_config.app_config import Base
+from configuration import ModelFieldError
 
 
 class BaseModel(Base):
@@ -12,8 +13,18 @@ class BaseModel(Base):
     def create(cls):
         return cls()
 
-    def as_dict(self, field_names):
+    def accept_read_visitor(self, field_names):
         instance = dict()
         for field in field_names:
             instance[field] = getattr(self, field)
         return instance
+
+    def accept_write_visitor(self, body):
+        for name in body:
+            try:
+                getattr(self, name)
+            except AttributeError:
+                raise ModelFieldError("Field {} is not listed for this model".format(name))
+            else:
+                setattr(self, name, body[name])
+        return self
