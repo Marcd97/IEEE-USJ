c29f1432415408ebdece02ddd0bd5a070c977b00
Marc Dib <marc.dib15@gmail.com>
2018-12-03 20:56:51 +0200

    FIxing the User Database

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Unicode, Integer, ForeignKey , String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Unicode
 from .base_model import BaseModel
 from .base_uid import UidModel, IdModel
 
-class User(UidModel,IdModel,BaseModel):
-    __tablename__ = 'User'
-    email=Column(String(20),nullable=True)
-    username=Column(String(20),nullabe=False)
-    phonenumber=Column(Integer,nullable=True)
+
+class User(UidModel, IdModel, BaseModel):
+    __tablename__ = 'users'
+    UID_PREFIX = 'USR'
+
+    email = Column(Unicode(255), nullable=True)
+    username = Column(Unicode(255), nullabe=False)
+    phone_number = Column(Unicode(255), nullable=True)
