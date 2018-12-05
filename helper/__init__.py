0074f6fd4c7e50dd882ad27632bb41944bfd906c
Marc Dib <marc.dib15@gmail.com>
2018-12-06 00:09:45 +0200

    Defined as_dict to help with the HTTP Arguments

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,1 +1,11 @@
 from .visitor import *
+
+
+def as_dict(filters):
+    filters_dict = dict(filters)
+    for key, value in filters_dict.items():
+        assert isinstance(value, list)
+        assert len(value) == 1
+        value = value[0]
+        filters_dict[key] = value
+    return filters_dict
