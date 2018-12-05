35930fe6f9b29b3b4973f5eabb5f399941ab6a10
Jana Asmar <asmarjana@yahoo.fr>
2018-12-05 13:35:39 +0200

    basic fixes

--- models/meeting.py
+++ models/meeting.py
@@ -1,22 +1,23 @@
-from sqlalchemy import Column, Unicode, Integer, ForeignKey , Date
+from sqlalchemy import Column, Unicode, Integer, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from .base_model import BaseModel
 from .base_uid import UidModel, IdModel
 
 
-class Meeting(UidModel,IdModel,BaseModel):
-    __tablename__='Meeting'
-    Date=Column(Date , nullable=False)
-    Location=Column(Unicode(255),nullable=False)
-    Title=Column(Unicode(255),nullable=False)
-    Description=Column(Unicode(255),nullable=False)
-    Priority=Column(Integer,nullable=False)
-    Requested_members=Column(Integer,nullable=False)
+class Meeting(UidModel, IdModel, BaseModel):
+    __tablename__ = 'meetings'
+    UID_PREFIX = 'MTG'
+    date = Column(Date, nullable=False)
+    location = Column(Unicode(255), nullable=False)
+    title = Column(Unicode(255), nullable=False)
+    description = Column(Unicode(255), nullable=False)
+    priority = Column(Integer, nullable=False)
+    requested_users = relationship('UserMeetingAssociation', cascade='all,delete')
 
 
-class UserMeetingAssociation(BaseModel,UidModel,IdModel):
-    __tablename__='UserMeeting'
-    meeting_id=Column(Integer,ForeignKey('meeting.id',ondelete='CASCADE'),nullable=False)
-    user_id=Column(Integer,ForeignKey('user.id',ondelete='CASCADE'),nullable=False)
-    user=relationship('User')
-    meeting=relationship('Meeting')
+class UserMeetingAssociation(BaseModel):
+    __tablename__ = 'user_meeting_assoc'
+    meeting_id = Column(Integer, ForeignKey('meeting.id', ondelete='CASCADE'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = relationship('User')
+    meeting = relationship('Meeting')
