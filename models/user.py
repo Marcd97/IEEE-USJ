4f783af0b3eda8a8ed87603781d5fc1bfd3e784e
Marc Dib <marc.dib15@gmail.com>
2018-12-05 13:24:09 +0200

    Added First and Last name for the UserDB

--- models/user.py
+++ models/user.py
@@ -10,3 +10,5 @@ class User(UidModel, IdModel, BaseModel):
     email = Column(Unicode(255), nullable=True)
     username = Column(Unicode(255), nullabe=False)
     phone_number = Column(Unicode(255), nullable=True)
+    first_name = Column(Unicode(255), nullable=False)
+    last_name = Column(Unicode(255), nullable=False)
