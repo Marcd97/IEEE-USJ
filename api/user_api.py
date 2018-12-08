6a8bb76f7f2b5a34ae3fd67db0bbe2d435f7c00e
Marc Dib <marc.dib15@gmail.com>
2018-12-08 19:51:03 +0200

    User API Fix

--- api/user_api.py
+++ api/user_api.py
@@ -1,25 +1,25 @@
 from flask import request, jsonify
 
-from helper.SimpleCrudHelper import SimpleCrudHelper
+from helper import as_dict
+from helper.simple_helper import SimpleCrudHandler
 from models.user import User
 
-handler = SimpleCrudHelper(User)
+handler = SimpleCrudHandler(User)
 
 
-class ModelApiExtension:
+class UserApiExtension:
     def __init__(self, app):
         if app is not None:
             self.init_app(app)
 
     @staticmethod
     def init_app(app):
-        app.add_url_rule('/path/users/', 'create_user', create_user, methods=['POST'])
-        app.add_url_rule('/path/users/<user_uid>', 'read_user', read_user, methods=['GET'])
-        app.add_url_rule('/path/users/<model_uid>', 'update_user', update_user, methods=['PUT'])
-        app.add_url_rule('/path/users/<model_uid>', 'delete_user', delete_user, methods=['DELETE'])
-        app.add_url_rule('/path/users/', 'search_user', search_user, methods=['GET'])
-        app.add_url_rule('/path/users/', 'bulk_update_user', bulk_update_user, methods=['PUT'])
-        app.add_url_rule('/path/users/', 'bulk_update_user', bulk_update_user, methods=['PUT'])
+        app.add_url_rule('/users', 'create_user', create_user, methods=['POST'])
+        app.add_url_rule('/users/<user_uid>', 'read_user', read_user, methods=['GET'])
+        app.add_url_rule('/users/<user_uid>', 'update_user', update_user, methods=['PUT'])
+        app.add_url_rule('/users/<user_uid>', 'delete_user', delete_user, methods=['DELETE'])
+        app.add_url_rule('/users', 'search_user', search_user, methods=['GET'])
+        app.add_url_rule('/users', 'bulk_update_user', bulk_update_user, methods=['PUT'])
 
 
 def create_user():
@@ -45,7 +45,7 @@ def delete_user(user_uid):
 
 
 def search_user():
-    filters = dict(request.args)
+    filters = as_dict(request.args)
     jsonable_dict, code = handler.search(filters)
     return jsonify(jsonable_dict), code
 
